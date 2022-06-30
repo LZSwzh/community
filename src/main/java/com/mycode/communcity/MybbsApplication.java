2f56a3caabd92278e07e16aa523e9eaa72697ae3
wzh <18403909715@163.com>
2022-06-30 10:58:31 +0800

    牛客论坛第一节——首页显示及分页功能实现(测试git)

--- src/main/java/com/mycode/communcity/MybbsApplication.java
+++ src/main/java/com/mycode/communcity/MybbsApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 public class MybbsApplication {
-
+    /**
+     * 加了个测试用于测试git
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(MybbsApplication.class, args);
     }
